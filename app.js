69287c57c1ac2c629b5fd0a741e559ea847276f3
Joe Spedale <jspedale@gmail.com>
2017-07-26 09:05:39 -0400

    Scaffolded basic app structure.

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
+app.set('port', (process.env.PORT || 8000));
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/:url', (req, res) => {
+  res.send({"url": req.params.url});
+});
+
+app.listen(app.get('port'), () => {
+  console.log('listening on port', app.get('port'));
+});
